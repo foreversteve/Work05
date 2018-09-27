be3c04dea0f283efa18199dd373cd81004a76ce1
Steven Liu <hliu4@stuy.edu>
2018-09-27 11:02:44 -0400

    Added Files

---
+++ Work05.c
@@ -0,0 +1,27 @@
+#include "Work05.h"
+
+int strlen(char *s){
+	int len = 0;
+	while (*s){
+		len++;
+	}
+	return len;
+}
+char *strncpy(char *dest, char *source, int n){
+	for (int i = 0; i < n; i++){
+		
+	}
+	return dest;
+}
+char *strcat(char *dest, char *source){
+	return dest;
+}
+int strcmp(char *s1, char *s2){
+
+}
+char *strchr(char *s, char c){
+
+}
+char *strstr(char *s1, char *s2){
+
+}
---
+++ Work05.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int strlen(char *s);
+char *strncpy(char *dest, char *source, int n);
+char *strcat(char *dest, char *source);
+int strcmp(char *s1, char *s2);
+char *strchr(char *s, char c);
+char *strstr(char *s1, char *s2);
